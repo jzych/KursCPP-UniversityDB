3c47e2a6ebd72bb39cb34b20e3e5408596fad537
majkel <michal.sobotka.84@gmail.com>
2019-01-16 22:17:15 +0100

    Add program main loop: 1. menuShow 2. menuChoose

--- main.cpp
+++ main.cpp
@@ -2,7 +2,53 @@
 
 using namespace std;
 
+void menuShow();
+void menuChoose();
+
 int main() {
-	cout << "Hello fellow Students!" << endl;
-	return 0;
+    do {
+    menuShow();
+    } while (true);
+    menuChoose();
+
+    return 0;
+}
+
+void menuShow() {
+	    system("cls");
+	    cout << "Program option:" << endl;
+	    cout << "1. Show students list" << endl;
+	    cout << "2. Add new student" << endl;
+	    cout << "3. Remove student" << endl;
+	    cout << "4. Sort students list" << endl;
+	    cout << "5. Exit program" << endl;
+}
+
+void menuChoose() {
+    char option;
+
+    do { 
+        cin >> option;
+
+        switch (option) {
+            case '1' :
+                           cout << "Showing students list" << endl;
+                           break;
+            case '2' : 
+                           cout << "Adding new student" << endl;
+                           break;
+            case '3' : 
+                           cout << "Removing student" << endl;
+                           break;
+            case '4' : 
+                           cout << "Sorting student list" << endl;
+                           break;
+            case '5' : 
+                           cout << "Finishing program" << endl;
+                           return;
+            default : 
+                          cout << "Unknown option " << option << "!" << endl;
+                          break;
+        }
+    } while (option != 5);
 }

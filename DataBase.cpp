d728f590b2c7e84f02e29448e6321a7a8d5dd04a
majkel <michal.sobotka.84@gmail.com>
2019-01-19 23:13:11 +0100

    Adding Student 1. getting data from console 2. pushing it to vector
    Edited main.cpp

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,8 +1,33 @@
 #include "DataBase.hpp"
+#include "Student.hpp"
+#include <iostream>
 #include <vector>
+#include <string>
+
+using namespace std;
 
 DataBase::DataBase()
 {
     std::vector<Student> dataBase;
 }
 
+void DataBase::inputData()
+{
+    string firstName;
+    string lastName;
+    int32_t studentID;
+    cout << "Enter student first name: ";
+    cin >> firstName;
+    cout << "Enter student last name:  ";
+    cin >> lastName;
+    cout << "Enter student ID no: ";
+    cin >> studentID;
+
+    addStudent(firstName, lastName, studentID);
+}
+
+void DataBase::addStudent(string & fn, string & ln, int32_t & in)
+{
+    student = new Student(fn, ln, in);
+    dataBase.push_back(student);
+}
